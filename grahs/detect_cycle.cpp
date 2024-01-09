7eb3c363a5c59008c1d9139e83e2c2a24c77436d
Ayoob Bhat <ayoob324005@gmail.com>
2024-01-09 13:06:23 +0530

    detect_cycles

--- grahs/detect_cycle.cpp
+++ grahs/detect_cycle.cpp
@@ -1,92 +1,55 @@
-class solution {
+class solution
+{
 
-    private :
+private:
+    bool detect(int src, vector<int> adj[], int vis[])
+    {
+        vis[src] = 1;
+        queue<pair<int, int>> q;
+        q.push({src, -1});
 
-    bool detect(int src,vector<int >adj[],int vis[]){
-        vis[src]=1;
-        queue<pair<int,int>>q;
-        q.push({src,-1});
-
-        while (q.empty()){
-            int node =q.front ().first();
-            int parent =q.front .second();
+        while (q.empty())
+        {
+            int node = q.front().first();
+            int parent = q.front.second();
             q.pop();
 
+            for (auto adjNode : adj[node])
+            {
 
-            for(auto adjNode:adj[node]){
+                if (!vist[adjNode])
+                {
+                    vis[adjNode] = 1;
 
-
-                if (!vist[adjNode]){
-                    vis[adjNode]=1;
-
-                    q.push({adjNode,node});
-
-
-
+                    q.push({adjNode, node});
                 }
-                else if(parent!=adjNode){
+                else if (parent != adjNode)
+                {
                     return true;
-
-
-
                 }
 
                 return false;
-
             }
-
-
-
-
-
-
         }
-
-
-    
-
-
-
     }
 
+public:
+    bool is Cycle(int v, vector<int> adj[])
+    {
 
-
-
-
-    public :
-
-    bool is Cycle(int v,vector <int>adj[]){
-
-
-        int vis[v]={0};
+        int vis[v] = {0};
 
         // if any componet has a cycle;
 
-        for (int i=0;i<v;v++){
-            if (!vis[i]){
-                if (detect(i,adj,vis))return true ;
-
-
-
+        for (int i = 0; i < v; v++)
+        {
+            if (!vis[i])
+            {
+                if (detect(i, adj, vis))
+                    return true;
             }
 
-            return false ;
-
-
-
-
-
-
+            return false;
         }
     }
-
-
-
-
-
-
-
-
-
-
 }
