9e65f2fb3711a0d97cc2e3b91da463f0c2fb6890
Ayoob Bhat <ayoob324005@gmail.com>
2024-03-02 12:01:26 +0530

    added sorting algo

---
+++ sorting_algo/insertion_sort.cpp
@@ -0,0 +1,31 @@
+
+
+
+# include <bits/stdc++.h>;
+
+using namespace std;
+
+
+void  insertion(int n, int arr[]){
+
+
+    for (int i=0;i<n-1;i++){
+
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+
+            swap(arr[j],arr[j+1]);
+            j--;
+            
+
+
+        }
+
+    }
+
+
+
+
+
+    int 
+}
